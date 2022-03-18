8921e12ace79e93fe790f6f08aff3f4abd89985e
sang567 <sangeethasangeetha025@gmail.com>
2022-03-18 19:45:38 +0530

    myprojectcommit

---
+++ spring-project/src/main/java/com/lti/project/springproject/SpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lti.project.springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+
+}
