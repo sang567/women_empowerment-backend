fbacbd4a6c44670d2cc0455aa860ee76d42c9391
sang567 <sangeethasangeetha025@gmail.com>
2022-03-20 12:00:46 +0530

    myfirstproject

--- spring-project/src/main/java/com/lti/controller/DocumentSectionController.java
+++ spring-project/src/main/java/com/lti/controller/DocumentSectionController.java
@@ -4,11 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import com.lti.entity.DocumentSection;
-
 import com.lti.service.DocumentSectionService;
 
 @RestController
@@ -18,7 +17,7 @@ public class DocumentSectionController {
 	@Autowired
 	private DocumentSectionService documentSectionService;
 	
-	@RequestMapping("/document.api")
+	@RequestMapping(path="/document/add" , method = RequestMethod.POST)
 	public String add(@RequestBody DocumentSection ds) {
 		documentSectionService.add(ds);
 		return "{\"status\" : \"Document Section Record Added Successfully!\"}";
